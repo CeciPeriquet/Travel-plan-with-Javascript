ed92a3ab1588992c7747ce73052fe0a91dbdbcfe
Cecilia Periquet <periwhat@gmail.com>
2022-11-11 21:05:09 +0100

    Const, Fetch and first functions

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,4 +3,37 @@
 const searchInput = document.querySelector('.js-input');
 const searchBtn = document.querySelector('.js-btn');
 const cardsList = document.querySelector('.js-list');
+
+let charactersList = [];
+
+function renderCards(charactersList) {
+    for (character of charactersList){
+    const card = `<li class="cards-list-item">
+    <article class="card">
+      <img src="${character.img}" alt="" class="card-img" />
+      <h2 class="card-name">${character.name}</h2>
+      <p class="card-status">${character.status}</p>
+    </article>
+  </li>;`
+    return card;
+}}
+
+function renderCharactersList (character){
+
+}
+
+fetch ('https://breakingbadapi.com/api/characters')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        charactersList = data.results;
+        console.log(charactersList);
+        renderCards(charactersList);
+    })
+
+function handleSearch (){
+
+}
+
+searchBtn.addEventListener('click', handleSearch);
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,43 @@
 const searchInput = document.querySelector('.js-input');
 const searchBtn = document.querySelector('.js-btn');
 const cardsList = document.querySelector('.js-list');
+
+let charactersList = [];
+
+function renderCharactersList (card) {
+    
+    for (const card of characters) {
+        cardsList.innerHTML = renderCards(card);
+        console.log(charactersList);
+    }
+  }
+
+function renderCards(character) {
+    
+    const card = `<li class="cards-list-item">
+    <article class="card">
+      <img src="${character.img}" alt="" class="card-img" />
+      <h2 class="card-name">${character.name}</h2>
+      <p class="card-status">${character.status}</p>
+    </article>
+  </li>;`
+    return card;
+}
+
+
+
+fetch ('https://breakingbadapi.com/api/characters')
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        charactersList = data.results;
+        console.log(charactersList);
+        renderCharactersList (data.results);
+
+    })
+
+function handleSearch (){
+
+}
+
+searchBtn.addEventListener('click', handleSearch);
