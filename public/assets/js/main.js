95cdd9f7b60e7f0e45573ad34862c271949fc5be
Cecilia Periquet <periwhat@gmail.com>
2022-11-11 20:24:57 +0100

    Update html and add basic css

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,6 @@
 'use strict';
 
-
+const searchInput = document.querySelector('.js-input');
+const searchBtn = document.querySelector('.js-btn');
+const cardsList = document.querySelector('.js-list');
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,5 @@
 'use strict';
 
+const searchInput = document.querySelector('.js-input');
+const searchBtn = document.querySelector('.js-btn');
+const cardsList = document.querySelector('.js-list');
