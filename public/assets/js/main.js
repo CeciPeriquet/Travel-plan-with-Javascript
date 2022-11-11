82c00d48db8157fbf7a98436ec39f4bd72b28aa5
Cecilia Periquet <periwhat@gmail.com>
2022-11-11 23:27:12 +0100

    Get data to html list and add somo more css

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,39 +1,60 @@
 'use strict';
 
+//CONSTANTES
+
+//Me traigo los elementos de html y los meto en variables
 const searchInput = document.querySelector('.js-input');
 const searchBtn = document.querySelector('.js-btn');
 const cardsList = document.querySelector('.js-list');
 
+// Declaro un array vacío donde luego meteré los datos de la API
 let charactersList = [];
 
-function renderCards(charactersList) {
-    for (character of charactersList){
-    const card = `<li class="cards-list-item">
+
+//FUNCIONES
+
+//Función para pintar las tarjetas de cada personaje (aún no funciona)
+function renderCharactersList () {
+    let characterCardList ='';
+    for (const card of charactersList) {
+        characterCardList += renderCards(card);
+    }
+    cardsList.innerHTML = characterCardList;
+  }
+
+//Función para pintar la tarjeta en sí, con sus elementos de html (aún no funciona)
+function renderCards(character) {
+    
+    let card = `<li class="cards-list-item">
     <article class="card">
       <img src="${character.img}" alt="" class="card-img" />
       <h2 class="card-name">${character.name}</h2>
       <p class="card-status">${character.status}</p>
     </article>
-  </li>;`
+  </li>`;
     return card;
-}}
-
-function renderCharactersList (character){
-
 }
 
+
+//Pido los datos a la API mediante Fetch (compruebo que me genera un array de objetos)
+function getData () {
 fetch ('https://breakingbadapi.com/api/characters')
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
-        charactersList = data.results;
-        console.log(charactersList);
-        renderCards(charactersList);
-    })
+        charactersList = data;
+        //no me estaba leyendo bien las funciones porque le pedía data.results y en este caso era solo data
+        renderCharactersList();
+    })}
 
 function handleSearch (){
 
 }
 
+//EVENTOS
+
+//Evento para cuando quiera escuchar al botón (aún no)
 searchBtn.addEventListener('click', handleSearch);
+
+//Al abrir la página, quiero los datos del API
+getData();
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //CONSTANTES
+
 //Me traigo los elementos de html y los meto en variables
 const searchInput = document.querySelector('.js-input');
 const searchBtn = document.querySelector('.js-btn');
@@ -13,38 +14,37 @@ let charactersList = [];
 //FUNCIONES
 
 //Función para pintar las tarjetas de cada personaje (aún no funciona)
-function renderCharactersList (card) {
-    
-    for (const card of characters) {
-        cardsList.innerHTML = renderCards(card);
-        console.log(charactersList);
+function renderCharactersList () {
+    let characterCardList ='';
+    for (const card of charactersList) {
+        characterCardList += renderCards(card);
     }
+    cardsList.innerHTML = characterCardList;
   }
 
 //Función para pintar la tarjeta en sí, con sus elementos de html (aún no funciona)
 function renderCards(character) {
     
-    const card = `<li class="cards-list-item">
+    let card = `<li class="cards-list-item">
     <article class="card">
       <img src="${character.img}" alt="" class="card-img" />
       <h2 class="card-name">${character.name}</h2>
       <p class="card-status">${character.status}</p>
     </article>
-  </li>;`
+  </li>`;
     return card;
 }
 
 
 //Pido los datos a la API mediante Fetch (compruebo que me genera un array de objetos)
+function getData () {
 fetch ('https://breakingbadapi.com/api/characters')
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
-        charactersList = data.results;
-        console.log(charactersList);
-        /* renderCharactersList (data.results); */
-
-    })
+        charactersList = data;
+        //no me estaba leyendo bien las funciones porque le pedía data.results y en este caso era solo data
+        renderCharactersList();
+    })}
 
 function handleSearch (){
 
@@ -54,3 +54,6 @@ function handleSearch (){
 
 //Evento para cuando quiera escuchar al botón (aún no)
 searchBtn.addEventListener('click', handleSearch);
+
+//Al abrir la página, quiero los datos del API
+getData();
