684f3fd031393ecb36513d67683fd9cc7e8c5234
Cecilia Periquet <periwhat@gmail.com>
2022-11-12 11:36:29 +0100

    Add favourites render functions and css

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@
 const searchInput = document.querySelector(".js-input");
 const searchBtn = document.querySelector(".js-btn");
 const cardsList = document.querySelector(".js-list");
+const favCardsList = document.querySelector(".js-fav-list");
 
 // Declaro un array vacío donde luego meteré los datos de la API
 let charactersList = [];
@@ -19,7 +20,7 @@ function renderCards(character) {
   let card = `<li class="cards-list-item">
     <article class="card js-card" id="${character.char_id}">
       <img src="${character.img}" alt="Picture of ${character.name}" title="${character.name}" class="card-img" />
-      <h2 class="card-name">${character.name}</h2>
+      <h3 class="card-name">${character.name}</h3>
       <p class="card-status">${character.status}</p>
     </article>
   </li>`;
@@ -28,33 +29,58 @@ function renderCards(character) {
 }
 
 //Función para pintar las tarjetas de cada personaje
-function renderCharactersList() {
-  let characterCardList = "";
-  for (const card of charactersList) {
-    characterCardList += renderCards(card);
+function renderCharactersList () {
+    let characterCardList ='';
+    for (const card of charactersList) {
+        characterCardList += renderCards(card);
+    }
+    cardsList.innerHTML = characterCardList;
+    cardListeners ();
+    
   }
-  cardsList.innerHTML = characterCardList;
-  cardListeners();
+
+//Función para pintar la tarjeta en sí, esta vez para los personajes favoritos
+function renderFavCard(favCharacter) {
+    let favCard = `<li class="cards-list-item">
+    <article class="card js-card" id="${favCharacter.char_id}">
+      <img src="${favCharacter.img}" alt="Picture of ${favCharacter.name}" title="${favCharacter.name}" class="card-img" />
+      <h3 class="card-name">${favCharacter.name}</h3>
+      <p class="card-status">${favCharacter.status}</p>
+    </article>
+  </li>`;
+    console.log(favCard);
+    console.log(favCharacter.name);
+  return favCard;
 }
 
+//Función para pintar las tarjetas de mis favoritos
+
+function renderFavCharacters(){
+let favCharacterCardList = "";
+  for (const card of favouriteCharacters) {
+    favCharacterCardList += renderFavCard(card);
+  }
+  favCardsList.innerHTML = favCharacterCardList;
+  console.log(favCardsList);
+}
+
+
+
+
+// Función para buscar los obj seleccionados y generar un nuevo array con ellos
 function handleClickCard(event) {
   event.currentTarget.classList.toggle("selected");
-  /* console.log(event.currentTarget.id); */
   const current = parseInt(event.currentTarget.id);
- /*  console.log(typeof current); */
-  
 
   const selectedCard = charactersList.find(
     (eachCardObj) => eachCardObj.char_id === current
-        //busco el problema y era que una era string y otra number, usando typeof 
-        /* const currentCard = eachCardObj.char_id;
-        console.log(currentCard);
-        console.log(typeof currentCard); */
-    );
-  
-    console.log(selectedCard);
-    favouriteCharacters.push(selectedCard);
-    console.log(favouriteCharacters);
+    //Busco el problema y era que una era string y otra number, uso typeof, por eso creo una variable donde recoger el nuevo valor (en nº) para comparar
+  );
+
+  console.log(selectedCard);
+  favouriteCharacters.push(selectedCard);
+  console.log(favouriteCharacters);
+  renderFavCharacters();
 }
 
 //Función que crea un bucle para recorrer los elementos del array generado con QSA y así nos permite aplicarle el eventListener
