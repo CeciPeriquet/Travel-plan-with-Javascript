b309ca50925932c573f31763b303a2f0ec031561
Cecilia Periquet <periwhat@gmail.com>
2022-11-12 20:15:00 +0100

    Add search button functions to filter by character name

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
 "use strict";
 
-//CONSTANTES
+//CONSTANTES GLOBALES
 
-//Me traigo los elementos de html y los meto en variables
+//Traigo los elementos de html y los guardo en variables
 const searchInput = document.querySelector(".js-input");
 const searchBtn = document.querySelector(".js-btn");
 const cardsList = document.querySelector(".js-list");
 const favCardsList = document.querySelector(".js-fav-list");
 
-// Declaro un array vacío donde luego meteré los datos de la API
+// Declaro un array vacío donde luego irán los datos de la API
 let charactersList = [];
 
 //Array de favoritos (también vacío) a partir de las tarjetas seleccionadas
@@ -29,7 +29,7 @@ function renderCards(character) {
   return card;
 }
 
-//Función para pintar las tarjetas de cada personaje
+//Función para pintar la lista completa de tarjetas de cada personaje
 function renderCharactersList() {
   let characterCardList = "";
   for (const card of charactersList) {
@@ -52,44 +52,13 @@ function renderFavCard(favCharacter) {
   return favCard;
 }
 
-//Función para pintar las tarjetas de mis favoritos
-
+//Función para pintar el listado de tarjetas de mis favoritos
 function renderFavCharacters() {
   let favCharacterCardList = "";
   for (const card of favouriteCharacters) {
     favCharacterCardList += renderFavCard(card);
   }
   favCardsList.innerHTML = favCharacterCardList;
-}
-
-// Función para buscar los obj seleccionados y generar un nuevo array con ellos
-function handleClickCard(event) {
-  event.currentTarget.classList.toggle("selected");
-  const current = parseInt(event.currentTarget.id);
-
-  const selectedCard = charactersList.find(
-    (eachCardObj) => eachCardObj.char_id === current);
-    //Busco el problema y era que una era string y otra number, uso typeof, por eso creo una variable donde recoger el nuevo valor (en nº) para comparar
-  
-  const cardFavouriteIndex = favouriteCharacters.findIndex(
-    (eachCardObj) => eachCardObj.char_id === current);
-  
-  //Si no está en favoritos, haz el push, si ya está, elimínala
-  if (cardFavouriteIndex === -1) { 
-    favouriteCharacters.push(selectedCard);
-  } else {
-    favouriteCharacters.splice(cardFavouriteIndex, 1);
-  }
-
-  renderFavCharacters();
-}
-
-//Función que crea un bucle para recorrer los elementos del array generado con QSA y así nos permite aplicarle el eventListener
-function cardListeners() {
-  const allCharacterCards = document.querySelectorAll(".js-card");
-  for (const eachCard of allCharacterCards) {
-    eachCard.addEventListener("click", handleClickCard);
-  }
 }
 
 //Pido los datos a la API mediante Fetch (compruebo que me genera un array de objetos)
@@ -103,12 +72,64 @@ function getData() {
     });
 }
 
-function handleSearch() {}
+//Función para filtrar según lo que se escriba en el input (una vez dado al botón)
+function filterCards() {
+  const searchedCharacter = searchInput.value.toLowerCase();
+  cardsList.innerHTML = "";
+
+  const filteredCharacters = charactersList
+    .filter((character) =>
+      character.name.toLowerCase().includes(searchedCharacter)
+    )
+
+  for (const character of filteredCharacters) {
+     const filteredCard = renderCards(character);
+     cardsList.innerHTML += filteredCard;
+  }
+}
 
 //EVENTOS
 
-//Evento para cuando quiera escuchar al botón (aún no)
+//Función que crea un bucle para recorrer los elementos del array generado con QSA y así nos permite aplicarle el eventListener a cada tarjeta
+function cardListeners() {
+  const allCharacterCards = document.querySelectorAll(".js-card");
+  for (const eachCard of allCharacterCards) {
+    eachCard.addEventListener("click", handleClickCard);
+  }
+}
+
+// Función para buscar los obj seleccionados y generar un nuevo array con ellos
+function handleClickCard(event) {
+  event.currentTarget.classList.toggle("selected");
+  const current = parseInt(event.currentTarget.id);
+
+  const selectedCard = charactersList.find(
+    (eachCardObj) => eachCardObj.char_id === current
+  );
+  //Busco el problema y era que una era string y otra number, uso typeof, por eso creo una variable donde recoger el nuevo valor (en nº) para comparar
+
+  const cardFavouriteIndex = favouriteCharacters.findIndex(
+    (eachCardObj) => eachCardObj.char_id === current
+  );
+
+  //Si no está en favoritos, haz el push, si ya está, elimínala
+  if (cardFavouriteIndex === -1) {
+    favouriteCharacters.push(selectedCard);
+  } else {
+    favouriteCharacters.splice(cardFavouriteIndex, 1);
+  }
+
+  renderFavCharacters();
+}
+
+//Función manejadora del botón de buscar, que nos lleva a la función de filtrado
+function handleSearch(event) {
+  event.preventDefault();
+  filterCards();
+}
+
+//Evento para escuchar al botón de buscar
 searchBtn.addEventListener("click", handleSearch);
 
-//Al abrir la página, quiero los datos del API
+//Al abrir la página, quiero los datos de la API
 getData();
