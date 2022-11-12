75484c0e6180dd93f756eba57ac48c5969f455e1
Cecilia Periquet <periwhat@gmail.com>
2022-11-12 11:01:01 +0100

    Add selected items new array

--- src/js/main.js
+++ src/js/main.js
@@ -1,75 +1,87 @@
-'use strict';
+"use strict";
 
 //CONSTANTES
 
 //Me traigo los elementos de html y los meto en variables
-const searchInput = document.querySelector('.js-input');
-const searchBtn = document.querySelector('.js-btn');
-const cardsList = document.querySelector('.js-list');
-
+const searchInput = document.querySelector(".js-input");
+const searchBtn = document.querySelector(".js-btn");
+const cardsList = document.querySelector(".js-list");
 
 // Declaro un array vacío donde luego meteré los datos de la API
 let charactersList = [];
 
+let favouriteCharacters = [];
 
 //FUNCIONES
 
 //Función para pintar la tarjeta en sí, con sus elementos de html
 function renderCards(character) {
-    
-    let card = `<li class="cards-list-item">
-    <article class="card js-card">
+  let card = `<li class="cards-list-item">
+    <article class="card js-card" id="${character.char_id}">
       <img src="${character.img}" alt="Picture of ${character.name}" title="${character.name}" class="card-img" />
       <h2 class="card-name">${character.name}</h2>
       <p class="card-status">${character.status}</p>
     </article>
   </li>`;
-    return card;
+
+  return card;
 }
 
 //Función para pintar las tarjetas de cada personaje
-function renderCharactersList () {
-    let characterCardList ='';
-    for (const card of charactersList) {
-        characterCardList += renderCards(card);
-    }
-    cardsList.innerHTML = characterCardList;
-    cardListeners ();
-    
+function renderCharactersList() {
+  let characterCardList = "";
+  for (const card of charactersList) {
+    characterCardList += renderCards(card);
   }
+  cardsList.innerHTML = characterCardList;
+  cardListeners();
+}
 
+function handleClickCard(event) {
+  event.currentTarget.classList.toggle("selected");
+  /* console.log(event.currentTarget.id); */
+  const current = parseInt(event.currentTarget.id);
+ /*  console.log(typeof current); */
+  
 
-function handleClickCard (event){
-    console.log('holi');
+  const selectedCard = charactersList.find(
+    (eachCardObj) => eachCardObj.char_id === current
+        //busco el problema y era que una era string y otra number, usando typeof 
+        /* const currentCard = eachCardObj.char_id;
+        console.log(currentCard);
+        console.log(typeof currentCard); */
+    );
+  
+    console.log(selectedCard);
+    favouriteCharacters.push(selectedCard);
+    console.log(favouriteCharacters);
 }
 
 //Función que crea un bucle para recorrer los elementos del array generado con QSA y así nos permite aplicarle el eventListener
-function cardListeners (){
-    const allCharacterCards = document.querySelectorAll('.js-card');
-    for (const eachCard of allCharacterCards) {
-        eachCard.addEventListener('click', handleClickCard);
-    }
+function cardListeners() {
+  const allCharacterCards = document.querySelectorAll(".js-card");
+  for (const eachCard of allCharacterCards) {
+    eachCard.addEventListener("click", handleClickCard);
+  }
 }
-
 
 //Pido los datos a la API mediante Fetch (compruebo que me genera un array de objetos)
-function getData () {
-fetch ('https://breakingbadapi.com/api/characters')
+function getData() {
+  fetch("https://breakingbadapi.com/api/characters")
     .then((response) => response.json())
     .then((data) => {
-        charactersList = data;
-        //no me estaba leyendo bien las funciones porque le pedía data.results y en este caso era solo data
-        renderCharactersList();
-    })}
-
-function handleSearch (){
-
+      charactersList = data;
+      //no me estaba leyendo bien las funciones porque le pedía data.results y en este caso era solo data
+      renderCharactersList();
+    });
 }
+
+function handleSearch() {}
 
 //EVENTOS
 
 //Evento para cuando quiera escuchar al botón (aún no)
-searchBtn.addEventListener('click', handleSearch);
+searchBtn.addEventListener("click", handleSearch);
 
 //Al abrir la página, quiero los datos del API
 getData();
